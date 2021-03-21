3178732c0fc49c99bf12674d7383677596726943
Danya <danila.boickov@icloud.com>
2021-03-21 15:31:37 +0300

    Crypto__step__13
    Added JS for calc

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,77 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Dropdown in Trade Section
 
-    const dropdown = document.querySelector('.trade__th');
+    'use strict';
 
-    dropdown.addEventListener('click', () => {
-        dropdown.querySelector('.trade__dropdown').classList.toggle('displaynone');
+    const dropdownParent = document.querySelector('.trade__th'),
+          hashrate = dropdownParent.querySelector('.trade__hr'),
+          dropdown = document.querySelector('.trade__dropdown');
+    
+
+    dropdownParent.addEventListener('click', () => {
+        dropdown.classList.toggle('displaynone');
     });
+
+    // trade calc 
+
+    const dropdownItems = dropdown.querySelectorAll('.trade__dropdown-item'),
+          inputHash = document.querySelector('.trade__hashrate'),
+          calcButton = document.querySelector('.trade__calculate'),
+          tradeETH = document.querySelector('.trade__ethTXT'),
+          trade__USD = document.querySelector('.usd');
+
+
+    let tradeOptions = {
+        hashrate: 'TH/S',
+        ratio: 0.2412,
+        usdValue: 300,
+
+        changeHashrate(hr) {
+            if (hr == 'TH/s') {
+                this.ratio = 0.2412;
+            } else if (hr == 'MH/s') {
+                this.ratio = 0.35; 
+            } else if (hr == 'GH/s') {
+                this.ratio = 0.4;
+            } else if (hr == 'PH/s') {
+                this.ratio = 0.5;
+            } else if (hr == 'EH/s') {
+                this.ratio = 0.6;
+            } else if (hr == 'KH/s') {
+                this.ratio = 0.2;
+            }
+        }
+    }
+
+    dropdown.addEventListener('click', (e) => {
+        if (e.target.classList.contains('trade__dropdown-item')) {
+            dropdownItems.forEach(item => {
+                if (item == e.target) {
+                    hashrate.textContent = `${item.textContent}`;
+                    tradeOptions.changeHashrate(`${item.textContent}`);
+                }
+            });
+        }
+    });
+
+    function updateRevenue() {
+        tradeETH.textContent = `${tradeOptions.ratio * inputHash.value} `;
+        trade__USD.textContent = `$${tradeETH.textContent * tradeOptions.usdValue}`;
+    }
+
+    calcButton.addEventListener('click', () => {
+        updateRevenue();
+    });
+
+    document.body.addEventListener('keydown', (e) => {
+        if (e.code == 'Enter' && (inputHash.value != '')) {
+            updateRevenue();
+        }
+    });
+
+
+
+
 
     // menu 
 
