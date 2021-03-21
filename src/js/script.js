8a2aae71c629a3ec52fd3a815a846d78b65e6303
Danya <danila.boickov@icloud.com>
2021-03-21 13:59:24 +0300

    Crypto__step__12
    Layout ended
    Respons ended...
    Added piano in the footer

--- src/js/script.js
+++ src/js/script.js
@@ -60,4 +60,22 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    // piano 
+
+    const footerLinks = document.querySelector('.footer__links'),
+          footerResources = document.querySelector('.footer__resources');
+    
+    function piano(item, otherItem) {
+        item.addEventListener('click', () => {
+            item.classList.toggle('piano');
+            if (otherItem.classList.contains('piano')) {
+                otherItem.classList.remove('piano');
+            }
+        });
+        
+    }
+
+    piano(footerLinks, footerResources);
+    piano(footerResources, footerLinks);
+
 });
